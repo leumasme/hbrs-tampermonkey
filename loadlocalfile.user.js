0ddad716c3b37a3a2170a911926d98a7f25f9fb4
Temm <samumert@gmail.com>
2022-09-21 18:31:19 +0200

    Add local file loading userscript

---
+++ loadlocalfile.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Local File
+// @version      0.1
+// @description  Your in-dev script
+// @author       Temm
+// @match        https://eva2.inf.h-brs.de/stundenplan/anzeigen/*mode=grid*
+// @require      file://C:\Path\To\file.user.js
+// @grant        none
+// ==/UserScript==
+
+// This script does nothing, it just loads another script from a local file.
+// This is useful while developing scripts, as you can edit the local file
+// with an external editor and then reload the page to see the changes.
