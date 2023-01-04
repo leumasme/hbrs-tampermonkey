e326635b88dd3d42a9f9de7b6849360ed967b10b
Temm <samumert@gmail.com>
2023-01-04 01:47:57 +0100

    [lea] Add Lea AutoLogin

---
+++ lealogin.user.js
@@ -0,0 +1,54 @@
+// ==UserScript==
+// @name         LEA Login
+// @version      1.0
+// @description  Your in-dev script
+// @author       Temm
+// @match        http*://lea.hochschule-bonn-rhein-sieg.de/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=h-brs.de
+// @grant        none
+// ==/UserScript==
+
+console.log("LEA Login Tampermonkey Loaded");
+let path = new URL(document.location).pathname;
+if (path == "/login.php") {
+    if (localStorage.username && localStorage.password) {
+        console.log("Logging in with stored credentials");
+        loginLea(localStorage.username, localStorage.password).then(()=> location.reload())
+    } else {        
+        // Login button has property name="cmd[doStandardAuthentication]"
+        const loginBtn = document.querySelector("input[name='cmd[doStandardAuthentication]']");
+
+        loginBtn.addEventListener("click", (e) => {
+            localStorage.username = document.getElementById("username").value;
+            localStorage.password = document.getElementById("password").value;
+        })
+    }
+}
+
+if (path == "/logout.php") {
+    localStorage.removeItem("username");
+    localStorage.removeItem("password");
+}
+
+async function loginLea(username, password) {
+    console.log(localStorage.username, localStorage.password, username, password)
+    let url = "https://lea.hochschule-bonn-rhein-sieg.de/ilias.php?lang=de&client_id=db_040811&cmd=post&cmdClass=ilstartupgui&cmdNode=yh&baseClass=ilStartUpGUI&rtoken="
+    let res = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            "origin": "null"
+        },
+        body: "cmd[doStandardAuthentication]=Anmelden&username=" + username + "&password=" + password,
+    });
+    if (!res.redirected) {
+        console.log("Login failed: "+res.status, res.redirected);
+        throw new Error("Login failed");
+    }
+    return true;
+}
+
+if (localStorage.username && localStorage.password) {
+    setInterval(() => loginLea(localStorage.username, localStorage.password), 1000 * 60 * 5);
+}
